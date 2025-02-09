07b153700b32dc023e0facaf94ca0bfbcb6e7bfd
Jenny Ding <53281037+dyyjenny@users.noreply.github.com>
2025-02-08 18:17:41 -0600

    Update svg_deg_filter_quest.py non-ct specific filter

--- data_analysis/drug_discovery/DGE/svg_deg_filter_quest.py
+++ data_analysis/drug_discovery/DGE/svg_deg_filter_quest.py
@@ -1,7 +1,3 @@
-### Author: Jenny
-###
-### Description: This script conducts protein-protein interaction analysis and builds interaction network for each cell type using spatially variable and differentially expressed genes. 
-
 import pandas as pd
 import os
 import sys
@@ -14,6 +10,11 @@ pid = 'PID'+dsid.split('DS')[1]
 dge_dir = '/projects/b1131/SpatialT/drug-target/'+dsid+'/'+sampleid+'/DGE_dec/'
 svg_dir = '/projects/b1131/SpatialT/'+tech+'/'+pid+'/'+dsid+'/'+sampleid+'/analysis/SVG/'
 sample_dge_dir = dge_dir.split('DGE_dec/')[0]
+
+svg = pd.read_csv(svg_dir+'SpatialDE_results.tsv', sep='\t')
+svg_filter = svg[svg['qval']<=0.1]
+print('svg read in')
+
 if not os.path.exists(sample_dge_dir+'DGE_dec_SVG'):
     os.makedirs(sample_dge_dir+'DGE_dec_SVG')
 
@@ -21,12 +22,9 @@ for file in os.listdir(dge_dir):
     cell_type = file.split('.txt')[0]
     print(cell_type)
     deg = pd.read_csv(dge_dir+file, sep='\t')
-    if os.path.isfile(svg_dir+'SpatialDE_results_'+cell_type+'.tsv'):
-        svg = pd.read_csv(svg_dir+'SpatialDE_results_'+cell_type+'.tsv', sep='\t')
-        deg=deg[deg['gene'].isin(svg['g'].tolist())]
-        print('SVG filtered')
-    else:
-        print('No SVG file')
+    deg=deg[deg['gene'].isin(svg_filter['g'].tolist())]
+    print('SVG filtered')
+
     df_up = deg[(deg['stat']>0.5) & (deg['qval']<0.05) ]
     df_down = deg[(deg['stat']<-0.5) & (deg['qval']<0.05) ]
     print('DEG up and down shape:', df_up.shape [0],df_down.shape[0])
